cc66f333038a63f722d76428b96beebfe8b6b196
Habtamu <habtamuwolde92@gmail.com>
2023-07-16 11:26:28 +0530

    added navbar for project

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,17 +1,28 @@
 export default function Nav(){
 
-  function handleClick(){
-    console.log("clicked")
+  function handleClick(e){
+    document.body.classList.toggle('nav-open')
   }
     return (
-        <nav>
-          <img src="../public/shared/logo.svg" className="logo" alt="logo"/>
-          <img 
-            src="../public/shared/icon-hamburger.svg" 
-            className="humburger" 
-            alt="hamburger"
-            onClick={handleClick}
-            /> 
+        <header>
+        <div class="logo">
+            <img src="../public/shared/logo.svg" />
+        </div>
+        <button 
+          class="nav-toggle" 
+          aria-label="toggle navigation"
+          onClick={handleClick}
+          >
+        <img src="../public/shared/icon-hamburger.svg" />
+        </button>
+        <nav class="nav">
+            <ul class="nav__list">
+                <li class="nav__item"><a href="#home" class="nav__link">Home</a></li>
+                <li class="nav__item"><a href="#services" class="nav__link">Crew</a></li>
+                <li class="nav__item"><a href="#about" class="nav__link">Technology</a></li>
+                <li class="nav__item"><a href="#work" class="nav__link">Destination</a></li>
+            </ul>
         </nav>
+    </header>
     )
 }
